2912cee241648ed34bfe13196f88cf16173c16f5
dunow <dunow33@hotmail.com>
2021-02-23 02:36:33 -0500

    all functions finishedgit add .

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Your code here
+function mapToNegativize(sourceArray){
+    let negArray = [];
+
+    for(let i of sourceArray)
+        negArray.push(i * -1);
+
+    return negArray;
+}
+
+function mapToNoChange(sourceArray){
+    return sourceArray;
+}
+
+function mapToDouble(sourceArray){
+    let dubArray = [];
+
+    for(let i of sourceArray)
+        dubArray.push(i * 2);
+
+    return dubArray;
+}
+
+function mapToSquare(sourceArray){
+    let sqArray = [];
+
+    for(let i of sourceArray)
+        sqArray.push(i ** 2);
+
+    return sqArray;
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0){
+    let total = startingPoint;
+
+    for(let i of sourceArray)
+        total += i;
+
+    return total;
+}
+
+function reduceToAllTrue(sourceArray){
+    for(let i of sourceArray)
+        if(!i)
+            return false;
+    return true;
+}
+
+function reduceToAnyTrue(sourceArray){
+    for(let i of sourceArray)
+        if(i)
+            return true;
+    return false;
+}
